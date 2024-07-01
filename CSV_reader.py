059774e053d2104766002bbb866d81e847c0a7b9
ongmj2011 <75373671+ongmj2011@users.noreply.github.com>
2024-07-02 00:35:31 +0800

    Update CSV_reader.py

--- CSV_reader.py
+++ CSV_reader.py
@@ -4,6 +4,11 @@ import pandas as pd
 class CSV_reader:
 
     @staticmethod
+
+    """
+    read the csv file and return a dataframe to be used in the subsequent classes 
+    """
+    
     def read_csv(file_path):
         data_frame = pd.read_csv(file_path)
         return data_frame
