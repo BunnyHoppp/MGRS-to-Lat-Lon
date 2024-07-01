1e34117d63773c73afc59cf14afaf6192e5c5d46
ongmj2011 <75373671+ongmj2011@users.noreply.github.com>
2024-07-01 15:47:47 +0800

    Update CSV_reader.py

--- CSV_reader.py
+++ CSV_reader.py
@@ -1,5 +1,18 @@
 import pandas as pd 
 
-class CSV_reader
-df = pd.read_csv(Wallaby Medium Input 1.csv)
+class CSVToDataFrame:
+    def __init__(self, file_path):
+        self.file_path = file_path
+        self.data_frame = None
+    
+    def read_csv(self):
+        self.data_frame = pd.read_csv(self.file_path)
+    
+    def display(self):
+        if self.data_frame is not None:
+            print(self.data_frame.head())
+
+csv_to_df = CSVToDataFrame('Wallaby Medium Input 1.csv')
+csv_to_df.read_csv()
+csv_to_df.display()
 
