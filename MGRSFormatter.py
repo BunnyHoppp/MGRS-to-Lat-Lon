f620fff70fa37208ad4bdb33ab7a10e9156a4ab7
ongmj2011 <75373671+ongmj2011@users.noreply.github.com>
2024-07-02 00:32:59 +0800

    Update MGRSFormatter.py

--- MGRSFormatter.py
+++ MGRSFormatter.py
@@ -5,6 +5,8 @@ class MGRSFormatter:
 
     """
     Purpose: Formatting our Latitude, Longitude read from the given map (data from csv file) into MGRS. 
+    MGRS: Grid_Zone + 100,000 meter Square ID + Numerical Coordinates 
+    
     Using DALLAS as a deployment area as an example: 
         Latitude, Longitude read from the given map (data from csv file): 101.5, 103.0 
         MGRS: 56KKA015030 
@@ -13,24 +15,32 @@ class MGRSFormatter:
         "56K" -> Grid Zone  
         "KA" -> 100,000 meter Square ID 
             we are focusing on the 4 Square IDs: JA, KA, JV, KV  
-            K - Latitude >100 
-            A - Longitude >100
-        "015030" -> Further zoomed in more within a 100 meter square 
-        "015" 
-        "030"
-            
-            
-        Numerical Coordinates: 
+            K: Latitude >100 
+            A: Longitude >100
+        "015030" -> Further zoomed in more within a 100 meter square (Numerical Coordinates) 
+            "015": Latitude 101.5 -> remove the first 1 (-100) and take out the decimal (*10)    
+            "030": Longitude 103                
         
         Reference: 
         https://mappingsupport.com/p2/gissurfer.php?center=14SQH05239974&zoom=4&basemap=USA_basemap
-        https://www.maptools.com/tutorials/mgrs/quick_guide
-        
-        
+        https://www.maptools.com/tutorials/mgrs/quick_guide                
+    """
+
+    """
+    MGRS: "Grid_Zone" + "100,000 meter Square ID" + "Numerical Coordinates" 
+    """
+    
+    """
+    mgrs_string (already added): None 
+    mgrs_string (to add): "Grid_Zone"
     """
     Grid_Zone = "56K"
-
+   
     @staticmethod
+    """
+    mgrs_string (already added): "Grid_Zone" 
+    mgrs_string (to add): "Grid_Zone"
+    """
     def mgrs_formatter(dataframe):
         number_of_points = len(dataframe)
         result = list()
