003fa23754e9a15980352d31a61e737f84f47eeb
ongmj2011 <75373671+ongmj2011@users.noreply.github.com>
2024-07-02 01:22:31 +0800

    Update MGRSConverter.py

--- MGRSConverter.py
+++ MGRSConverter.py
@@ -2,10 +2,21 @@ import mgrs
 
 
 class MGRSConverter:
+    """
+    Convert mgrs (string) into mgrs_obj 
+    """
     mgrs_obj = mgrs.MGRS()
 
     @staticmethod
     def mgrs_converter(mgrs_list):
+        """Convert a list of MGRS coordinates to a list of (Latitude, Longitude) using a helper function mgrs_tolatlon
+        
+        Args: 
+            mgrs_list: a list of MGRS coordinates  
+            
+        Returns:
+            result_list: a list of (Latitude, Longitude)  
+        """
         list_len = len(mgrs_list)
         result_list = list()
         for i in range(list_len):
@@ -16,5 +27,13 @@ class MGRSConverter:
 
     @staticmethod
     def mgrs_tolatlon(mgrs_string):
+        """Convert 1 mgrs_string to 1 (Latitude, Longitude)
+
+        Args: 
+            mgrs_string: MGRS Coordinate 
+
+        Returns: 
+            result: (Latitude, Longitude) 
+        """
         result = MGRSConverter.mgrs_obj.toLatLon(mgrs_string)
         return result
