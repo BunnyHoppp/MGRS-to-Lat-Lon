df6b4d83b364324d4541fd8ebf5d252b8fecdb92
ongmj2011 <75373671+ongmj2011@users.noreply.github.com>
2024-07-02 01:08:21 +0800

    Update CSV_reader.py

--- CSV_reader.py
+++ CSV_reader.py
@@ -5,10 +5,15 @@ class CSV_reader:
 
     @staticmethod
 
-    """
-    Read the csv file and return a dataframe to be used in the subsequent classes 
-    """
-    
     def read_csv(file_path):
+        """
+        Read the csv file and return a dataframe to be used in other classes 
+    
+        Args: 
+            file_path: csv file containing the latitude/longitude derived from the map 
+
+        Returns: 
+            data_frame
+        """
         data_frame = pd.read_csv(file_path)
         return data_frame
