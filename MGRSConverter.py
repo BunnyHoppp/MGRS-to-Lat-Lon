4b97b9bb62778800d6c6dcd144feb283025ae9c7
ongmj2011 <ongmj2011@gmail.com>
2024-07-02 15:11:43 +0800

    updated MGRS_converter
    same comments as previous version
    to cont with MGRS formatter more

--- MGRSConverter.py
+++ MGRSConverter.py
@@ -2,10 +2,24 @@ import mgrs
 
 
 class MGRSConverter:
+    """ Convert MGRS to (Lat,Lon)
+    Attributes: mgrs_obj
+    """
+    """
+    Convert mgrs (string) into mgrs_obj
+    """
     mgrs_obj = mgrs.MGRS()
 
     @staticmethod
     def mgrs_converter(mgrs_list):
+        """Convert a list of MGRS coordinates to a list of (Latitude, Longitude) using a helper function mgrs_tolatlon
+
+        Args:
+            mgrs_list: a list of MGRS coordinates
+
+        Returns:
+             result_list: a list of (Latitude, Longitude)
+        """
         list_len = len(mgrs_list)
         result_list = list()
         for i in range(list_len):
@@ -16,6 +30,14 @@ class MGRSConverter:
 
     @staticmethod
     def mgrs_tolatlon(mgrs_string):
+        """Convert 1 mgrs_string to 1 (Latitude, Longitude)
+
+        Args:
+            mgrs_string: MGRS Coordinate
+
+        Returns:
+            result: (Latitude, Longitude)
+        """
         if mgrs_string == None:
             return None
         result = MGRSConverter.mgrs_obj.toLatLon(mgrs_string)
--- MGRSFormatter.py
+++ MGRSFormatter.py
@@ -2,11 +2,42 @@ import pandas as pd
 
 
 class MGRSFormatter:
+    """Format Latitude, Longitude (from csv file) into MGRS.
+    MGRS: Grid_Zone + 100,000 meter Square ID + 100 meter square
+
+    Example (DALLAS deployment area):
+        Latitude, Longitude (csv input): 101.5, 103.0
+        MGRS: 56KKA015030
+
+    Explanation:
+        "56K" -> Grid Zone
+        "KA" -> 100,000 meter Square ID (i.e., JA, KA, JV, KV)
+            K: Latitude >100
+            A: Longitude >100
+        "015030" -> 100 meter square
+            "015": Latitude 101.5 -> remove the first 1 (-100) and take out the decimal (*10)
+            "030": Longitude 103
+
+        Reference:
+        https://mappingsupport.com/p2/gissurfer.php?center=14SQH05239974&zoom=4&basemap=USA_basemap
+        https://www.maptools.com/tutorials/mgrs/quick_guide
+    """
+
     Grid_Zone = "56K"
     Accuracy = 3
    
     @staticmethod
     def mgrs_formatter(dataframe):
+        """
+        Convert the Latitude and Longitude (csv input) to a list of mgrs_string
+        using the helper function mgrs_tostring
+
+        Args:
+            dataframe: Latitude and Longitude (read from csv file)
+
+        Returns:
+            result: a list of mgrs_string
+        """
         number_of_points = len(dataframe)
         result = list()
         for i in range(number_of_points):
@@ -18,6 +49,23 @@ class MGRSFormatter:
 
     @staticmethod
     def mgrs_tostring(latitude, longitude):
+        """ Convert 1 pair of latitude and longitude into 1 MGRS string using the helper function latlon_tostring.
+
+        Args:
+            latitude: Latitude value (top-down of map)
+            longitude: Longitude value (left-right of map)
+
+        Returns:
+            result_string: MGRS string
+
+        Steps to generate the output:
+            1. Initialize result_string with the Grid Zone.
+            2. Determine square_id (100,000 meter Square ID).
+            3. Determine a string of numerical values representing the 100-meter square where the point lies using latlon_tostring.
+               - Both latitude and longitude are evaluated individually.
+            4. Add to result_string in this sequence: square_id, longitude, latitude.
+               - Note: result_string is already initialized with Grid Zone from Step 1.
+        """
         if pd.isna(latitude) or pd.isna(longitude):
             return None
         result_string = MGRSFormatter.Grid_Zone
