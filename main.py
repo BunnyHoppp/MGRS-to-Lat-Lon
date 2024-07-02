f8e869719b0360905272fe14116ca531388e3d31
ongmj2011 <75373671+ongmj2011@users.noreply.github.com>
2024-07-02 10:05:31 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,34 +4,50 @@ from MGRSFormatter import MGRSFormatter
 class main:
 
     def __init__(self, file_path):
+        """ Initialisation 
+
+        Args: 
+            file_path: csv file  
+        """
         self.coordinate_data = file_path
         self.coordinate_list = None
         self.mgrs_list = None
         self.latlon_list = None
 
     def read_csv(self):
+        """ read the csv file containing latitude/longitude (top to bottom/ left to right in map)  
+        """
         print("start read")
         self.coordinate_list = CSV_reader.read_csv(self.coordinate_data)
         print("end read")
         print(self.coordinate_list)
 
     def format_mgrs(self):
+        """format the lat/lon (top to bottom / left to right in map) into appropriate mgrs format  
+        """
         print("start format")
         self.mgrs_list = MGRSFormatter.mgrs_formatter(self.coordinate_list)
         print("end format")
         print(self.mgrs_list)
 
     def convert_mgrs(self):
+        """convert mgrs to latitude/longitude
+        """
         print("start convert")
         self.latlon_list = MGRSConverter.mgrs_converter(self.mgrs_list)
         print("end convert")
 
     def run_whole_thing(self):
+        """
+        run everything: lat/lon in csv -> mgrs -> latitude/longitude 
+        """
         self.read_csv()
         self.format_mgrs()
         self.convert_mgrs()
         print(self.latlon_list)
 
 if __name__ == '__main__':
+    """run     
+    """
     thisrun = main('Test.csv')
     thisrun.run_whole_thing()
